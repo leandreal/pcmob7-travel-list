1fd1eec7d1b4a5a69d46cfe3b7bae679ef675fe4
Leandreal <leandrelim@gmail.com>
2022-08-25 21:05:19 +0800

    Add Starter Codes for Constants.js

---
+++ constants.js
@@ -0,0 +1,12 @@
+// API
+export const API = "https://pcmob6-blog-api.leal3.repl.co";
+export const API_LOGIN = "/auth";
+export const API_SIGNUP = "/newuser";
+export const API_POSTS = "/posts";
+export const API_WHOAMI = "/whoami";
+export const API_CREATE = "/create";
+
+// Screen names
+export const AUTH_SCREEN = "AUTH_SCREEN"
+export const PROFILE_SCREEN = "PROFILE_SCREEN"
+
