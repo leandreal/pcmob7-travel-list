31bb7a5b7b063e650b4f52b2e94a6d3e8b9e991a
Leandreal <leandrelim@gmail.com>
2022-08-27 16:37:05 +0800

    Add Profile Pic from Camera on ProfileScreen.js

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AUTH_SCREEN, API, API_WHOAMI, CAMERA_SCREEN } from "../constants";
 import axios from "axios";
 
+const imgPlaceholder = "https://picsum.photos/200";
+
+
 export default function ProfileScreen() {
   const navigation = useNavigation();
   const [username, setUsername] = useState("loading...");
+  const [photoUri, setPhotoUri] = useState(null);
+
+
+  async function loadPhoto() {
+    const photo = await AsyncStorage.getItem("photo_uri");
+    setPhotoUri(photo);
+  }
 
   async function loadUsername() {
     const token = await AsyncStorage.getItem("token");
@@ -22,8 +32,13 @@ export default function ProfileScreen() {
   }
 
   useEffect(() => {
-    const removeListener = navigation.addListener("focus", loadUsername);
-    loadUsername();
+    const removeListener = navigation.addListener("focus", () => {
+      loadUsername();
+      loadPhoto();
+    });
+
+      loadUsername();
+      loadPhoto();
     return () => {
       removeListener();
     };
@@ -35,6 +50,14 @@ export default function ProfileScreen() {
       <Text style={{ fontSize: 20, fontWeight: "700", marginBottom: 10 }}>
         your user name: {username}
       </Text>
+
+      <Image
+        source={{ uri: photoUri ?? imgPlaceholder }}
+        style={{ height: 120, width: 120, borderRadius: 3, marginBottom: 20}}
+        />
+
+
+
       <TouchableOpacity
         style={styles.outlinedButton}
         onPress={() => navigation.navigate(CAMERA_SCREEN)}
